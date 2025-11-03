04b0489acd813296f606759a5bb1d512b0d74dd8
Thant Thurein Htut <thantthureinhtut@Thants-MacBook-Pro.local>
2025-11-04 00:31:12 +0800

    Initial commit - Laravel API and Admin

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['https://snapblog.dpdns.org'],
 
     'exposed_headers' => [],
 
--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\Profile\ProfileEditController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 Route::get('/user/post' ,[PostController::class , 'posts']);
 Route::post('/user/post' ,[PostController::class , 'posts']);
